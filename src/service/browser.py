cf749c9995b68288a662cf3711ad73eba9d79f6a
ButkovEN <butkoven@sibur.ru>
2025-02-24 12:33:22 +0300

    add is_headless in get_webdriver

--- src/service/browser.py
+++ src/service/browser.py
@@ -3,14 +3,15 @@ from selenium import webdriver
 
 class BrowserService:
     @staticmethod
-    def get_webdriver():
+    def get_webdriver(is_headless: bool = True):
         """
         # from webdriver_manager.chrome import ChromeDriverManager
         # return webdriver.Chrome(executable_path=ChromeDriverManager().install())
         # https://sites.google.com/chromium.org/driver/downloads
         """
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
+        if is_headless:
+            options.add_argument("--headless=new")
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
