04b3b19762b45fd54583d6dc806dc756db8b57dd
evgeniy.butkov <butkovevg@gmail.com>
2025-10-14 14:06:59 +0300

    fix start-process1

--- src/api/scheduler.py
+++ src/api/scheduler.py
@@ -4,10 +4,14 @@ from typing import Annotated
 from fastapi import APIRouter, BackgroundTasks
 from pydantic import Field
 
-from scheduler import run_mode
 from src.configs.settings import settings
 from src.model.response import ResponseModel, StatusModel
-from src.model.types_of_sports import ModeEnum, TypesOfSportsModel, dct_rus_to_eng
+from src.model.types_of_sports import (
+    ModeEnum,
+    TypesOfSportsModel,
+    dct_rus_to_eng,
+)
+from src.service.scheduler import get_scheduler_service, run_mode
 
 router = APIRouter(
     prefix='/scheduler',
@@ -19,12 +23,13 @@ router = APIRouter(
 async def start_process(
         background_tasks: BackgroundTasks,
         rus_sport_name: TypesOfSportsModel = TypesOfSportsModel.volleyball,
-        day_number: Annotated[int, Field(ge=0, le=6)] = 0,
+        shift_day: Annotated[int, Field(ge=0, le=6)] = 0,
         hidden: bool = True,
         mode: ModeEnum = ModeEnum.main,
 ):
     settings.IS_HEADLESS = hidden
     eng_sport_name = dct_rus_to_eng.get(rus_sport_name.value)
-    background_tasks.add_task(run_mode, eng_sport_name, day_number, mode.value)
+    scheduler_service = get_scheduler_service(eng_sport_name=eng_sport_name, shift_day=shift_day)
+    background_tasks.add_task(run_mode, scheduler_service, mode.value)
     return ResponseModel(status=StatusModel.SUCCESS,
                          data=f"Фоновая задача запущена {datetime.utcnow().isoformat() + 'Z'}")
