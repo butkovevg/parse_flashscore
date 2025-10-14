84cbc7927b11907700a9ee621b1ea36e70292763
evgeniy.butkov <butkovevg@gmail.com>
2025-10-14 14:08:42 +0300

    fix SchedulerService

--- src/api/scheduler.py
+++ src/api/scheduler.py
@@ -11,7 +11,7 @@ from src.model.types_of_sports import (
     TypesOfSportsModel,
     dct_rus_to_eng,
 )
-from src.service.scheduler import get_scheduler_service, run_mode
+from src.service.scheduler import SchedulerService, get_scheduler_service, run_mode
 
 router = APIRouter(
     prefix='/scheduler',
@@ -29,7 +29,7 @@ async def start_process(
 ):
     settings.IS_HEADLESS = hidden
     eng_sport_name = dct_rus_to_eng.get(rus_sport_name.value)
-    scheduler_service = get_scheduler_service(eng_sport_name=eng_sport_name, shift_day=shift_day)
+    scheduler_service: SchedulerService = get_scheduler_service(eng_sport_name=eng_sport_name, shift_day=shift_day)
     background_tasks.add_task(run_mode, scheduler_service, mode.value)
     return ResponseModel(status=StatusModel.SUCCESS,
                          data=f"Фоновая задача запущена {datetime.utcnow().isoformat() + 'Z'}")
